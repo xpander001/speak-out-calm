392ae9ffe4499c6cdc235c0e76618fa6ba96acf1
Ander Suarez Martinez <me@andersuarez.com>
2018-02-24 14:51:34 +0000

    Add logo to canvas

--- src/components/CanvasElement/CanvasElement.js
+++ src/components/CanvasElement/CanvasElement.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './CanvasElement.css';
 
+import logo from './calm-logo-white.png';
+
 class CanvasElement extends React.Component {
   state = {  }
 
@@ -12,16 +14,22 @@ class CanvasElement extends React.Component {
       this.updateCanvas();
   }
   updateCanvas() {
-    const canvas = this.refs.canvas;
-    const {title, text, color} = this.props;
-    const ctx = this.refs.canvas.getContext('2d');
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
-    ctx.font="20px Georgia";
-    ctx.fillText(title,10,50);
-    ctx.font="20px Georgia";
-    ctx.fillText(text,10,100);
+    var imageObj = new Image();
+    const that = this;
+    imageObj.onload = function() {
+      const canvas = that.refs.canvas;
+      const {title, text, color} = that.props;
+      const ctx = that.refs.canvas.getContext('2d');
+      ctx.fillStyle = color;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = 'white';
+      ctx.font="20px Georgia";
+      ctx.fillText(title, canvas.width / 3, 50);
+      ctx.font="20px Georgia";
+      ctx.fillText(text, canvas.width / 3,100);
+      ctx.drawImage(imageObj, 10, 0);
+    };
+    imageObj.src = logo; 
   }
 
   render() {
