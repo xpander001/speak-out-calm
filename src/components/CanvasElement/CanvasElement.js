d6468c2b5a1708e01f56e70308c046340bbf4c1e
Bex B <bexb1@hotmail.com>
2018-02-24 17:03:46 +0000

    Canvas teaks

--- src/components/CanvasElement/CanvasElement.js
+++ src/components/CanvasElement/CanvasElement.js
@@ -20,13 +20,13 @@ class CanvasElement extends React.Component {
       const canvas = that.refs.canvas;
       const {title, text, color} = that.props;
       const ctx = that.refs.canvas.getContext('2d');
-      ctx.textAlign="center";
+      ctx.textAlign = "center";
       ctx.fillStyle = color;
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = 'white';
-      ctx.font="26px Verdana";
+      ctx.font = "36px Verdana";
       ctx.fillText(title, canvas.width / 2, 120);
-      ctx.font="18px Verdana";
+      ctx.font = "20px Verdana";
       ctx.fillText(text, canvas.width / 2,180);
       ctx.drawImage(imageObj, 10, 0);
     };
@@ -35,7 +35,7 @@ class CanvasElement extends React.Component {
 
   render() {
     return (
-      <canvas ref="canvas" width={470} height={290}/>
+      <canvas ref="canvas" width={670} height={400}/>
     )
   }
 }
