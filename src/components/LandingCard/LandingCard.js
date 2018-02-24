c36d569c3ea93389d555b07e6ae9e027ef98b31e
Bex B <bexb1@hotmail.com>
2018-02-24 15:24:36 +0000

    merging from master.

--- src/components/LandingCard/LandingCard.js
+++ src/components/LandingCard/LandingCard.js
@@ -6,7 +6,7 @@ class LandingCard extends React.Component {
       <div className="landing-container background--boundless-blue col-md-8 col-md-offset-2 margin--vertical-xl text--white text--center padding--md gap--md"> 
         <h1 className="text--xl landing-container__title">Share Your Man Feelings</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quisquam illo sequi, perspiciatis rem asperiores quae voluptatum laudantium natus consequatur, qui in enim ducimus at. Voluptatibus, ipsum reiciendis? Animi, labore!</p>
-        <p><a href="button">Create Postcard</a></p>
+        <p><a href="/dashboard" className="button">Create Postcard</a></p>
       </div>
     )
   }
