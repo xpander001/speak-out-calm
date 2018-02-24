96e330353c5eb121b62c69c20a834e661c687ee0
Ander Suarez Martinez <me@andersuarez.com>
2018-02-24 15:34:14 +0000

    State lift to Main component

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -4,64 +4,41 @@ import TextCardList from '../../components/TextCardList/TextCardList';
 import ColorList from '../../components/ColorList/ColorList';
 import CanvasElement from '../../components/CanvasElement/CanvasElement';
 
-import data from '../../data/data.json';
-
-class Dashboard extends React.Component {
-  
-  state = {
-    data,
-    selectedColor: data.colors[0],
-    selectedTitle: data.messages[0].title,
-    selectedText: data.messages[0].text,
-  }
-
-  onColorSelected =(color) => {
-    this.setState({selectedColor: color});
-  }
-  
-  onTextSelected = (title, text) => {
-    this.setState({
-      selectedTitle: title,
-      selectedText: text
-    });
-  }
-
-  render() {
-    return (
-      <section id="dashboard">
-        <div className="container">
-          <div className="row">
-            <h1 className="col-xs-12">Dashboard</h1>
-          </div>
+function Dahsboard(props) {
+  return (
+    <section id="dashboard">
+      <div className="container">
+        <div className="row">
+          <h1 className="col-xs-12">Dashboard</h1>
         </div>
-        <div className="container">
-          <div className="row">
-            <aside className="col-md-5">
-              <div className="row">
-                <h2>Choose your statement</h2>
-                <TextCardList
-                  messages={this.state.data.messages}
-                  onTextSelected={this.onTextSelected}
-                  selectedText={this.state.selectedText}
-                  />
-              </div>
-              <div className="row">
-                <h2>Colors</h2>
-                <ColorList
-                  colors={this.state.data.colors}
-                  onColorSelected={this.onColorSelected}
-                  selectedColor={this.state.selectedColor}
-                  />
-              </div>
-            </aside>
-            <main className="col-md-7">
-              <CanvasElement title={this.state.selectedTitle} text={this.state.selectedText} color={this.state.selectedColor} />
-            </main>
-          </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <aside className="col-md-5">
+            <div className="row">
+              <h2>Choose your statement</h2>
+              <TextCardList
+                messages={props.data.messages}
+                onTextSelected={props.onTextSelected}
+                selectedText={props.selectedText}
+                />
+            </div>
+            <div className="row">
+              <h2>Colors</h2>
+              <ColorList
+                colors={props.data.colors}
+                onColorSelected={props.onColorSelected}
+                selectedColor={props.selectedColor}
+                />
+            </div>
+          </aside>
+          <main className="col-md-7">
+            <CanvasElement title={props.selectedTitle} text={props.selectedText} color={props.selectedColor} />
+          </main>
         </div>
-      </section>
-    );
-  }
+      </div>
+    </section>
+  );
 }
 
-export default Dashboard;
+export default Dahsboard;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,18 +9,48 @@ import Review from "../pages/Review/Review";
 import Footer from "../components/Footer/Footer";
 import data from "../data/data.json";
 
-console.log(data);
+
 
 class Main extends React.Component {
-  state = {};
+  
+  state = {
+    data,
+    selectedColor: data.colors[0],
+    selectedTitle: data.messages[0].title,
+    selectedText: data.messages[0].text,
+  };
+
+  onColorSelected =(color) => {
+    this.setState({selectedColor: color});
+  }
+  
+  onTextSelected = (title, text) => {
+    this.setState({
+      selectedTitle: title,
+      selectedText: text
+    });
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div id="main">
           <Header />
-          <Route exact path="/" component={Landing} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/review" component={Review} />
+          <Route exact path="/" component={() => <Landing />} />
+          <Route path="/dashboard" component={
+              () => <Dashboard  onColorSelected={this.onColorSelected}
+                                onTextSelected={this.onTextSelected}
+                                data={this.state.data}
+                                selectedColor={this.state.selectedColor}
+                                selectedTitle={this.state.selectedTitle}
+                                selectedText={this.state.selectedText}
+                                />
+            } />
+          <Route path="/review" component={() => <Review
+                                                    selectedColor={this.state.selectedColor}
+                                                    selectedTitle={this.state.selectedTitle}
+                                                    selectedText={this.state.selectedText}
+                                                    />} />
           <Footer />
         </div>
       </BrowserRouter>
