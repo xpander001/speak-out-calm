93f7e7cb6f054bca1fd39f3b006746a86b1d1099
Gbaja <gbajaf@yahoo.co.uk>
2018-02-24 16:47:21 +0000

    fix merge conflicts

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -8,52 +8,53 @@ import Review from "../pages/Review/Review";
 import Footer from "../components/Footer/Footer";
 import data from "../data/data.json";
 
-
-
 class Main extends React.Component {
-
   state = {
     data,
     selectedColor: data.colors[0],
     selectedTitle: data.messages[0].title,
-    selectedText: data.messages[0].text,
+    selectedText: data.messages[0].text
   };
 
-  onColorSelected =(color) => {
-    this.setState({selectedColor: color});
-  }
+  onColorSelected = color => {
+    this.setState({ selectedColor: color });
+  };
 
   onTextSelected = (title, text) => {
     this.setState({
       selectedTitle: title,
       selectedText: text
     });
-  }
+  };
 
   render() {
     return (
       <BrowserRouter>
         <div id="main">
-          <Header />
           <Route exact path="/" component={() => <Landing />} />
-          <Route path="/dashboard" component={
-              () => <Dashboard  onColorSelected={this.onColorSelected}
-                                onTextSelected={this.onTextSelected}
-                                data={this.state.data}
-                                selectedColor={this.state.selectedColor}
-                                selectedTitle={this.state.selectedTitle}
-                                selectedText={this.state.selectedText}
-                                />
-            } />
-          <Route path="/review" component={() => <Review
-                                                    selectedColor={this.state.selectedColor}
-                                                    selectedTitle={this.state.selectedTitle}
-                                                    selectedText={this.state.selectedText}
-                                                    />} />
-        <div id="main" className="main">
-          <Route exact path="/" component={Landing} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/review" component={Review} />
+          <Route
+            path="/dashboard"
+            component={() => (
+              <Dashboard
+                onColorSelected={this.onColorSelected}
+                onTextSelected={this.onTextSelected}
+                data={this.state.data}
+                selectedColor={this.state.selectedColor}
+                selectedTitle={this.state.selectedTitle}
+                selectedText={this.state.selectedText}
+              />
+            )}
+          />
+          <Route
+            path="/review"
+            component={() => (
+              <Review
+                selectedColor={this.state.selectedColor}
+                selectedTitle={this.state.selectedTitle}
+                selectedText={this.state.selectedText}
+              />
+            )}
+          />
           <Footer />
         </div>
       </BrowserRouter>
